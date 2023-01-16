e015b5b896c9cb4dc28aecf9eb500094ddddefee
InverTome <104219631+invertome@users.noreply.github.com>
2023-01-16 13:27:07 -0700

    Update plotged.py

--- genealogy/plotged.py
+++ genealogy/plotged.py
@@ -37,45 +37,59 @@ for ancestor in ancestors:
     if args.clean:
         ax.text(ancestor.x_coord, ancestor.y_coord, ancestor.name)
     else:
-            # Get birth date, birth place, marriage date, marriage place, death date, death place
+        # Get birth date, birth place, marriage date, marriage place, death date, death place
+        birth_date = ancestor.get_birth_date().get_value() if ancestor.get_birth_date() else 'N/A'
+        birth_place = ancestor.get_birth_place().get_value() if ancestor.get_birth_place() else 'N/A'
+        marriage_date = ancestor.get_marriage_date().get_value() if ancestor.get_marriage_date() else 'N/A'
+        marriage_place = ancestor.get_marriage_place().get_value() if ancestor.get_marriage_place() else 'N/A'
+        death_date = ancestor.get_death_date().get_value() if ancestor.get_death_date() else 'N/A'
+        death_place = ancestor.get_death_place().get_value() if ancestor.get_death_place() else 'N/A'
+        ax.text(ancestor.x_coord, ancestor.y_coord, ancestor.name + ": " + birth_date + ' ' + birth_place + ' ' + marriage_date + ' ' + marriage_place + ' ' + death_date + ' ' + death_place)
+
+    # Handle repeated ancestors due to endogamy
+repeated_ancestors = set()
+for ancestor in ancestors:
+    if ancestor.get_id().get_value() in repeated_ancestors:
+        # Link ancestor to itself with a faint line
+        ax.annotate("", xy=(ancestor.x_coord, ancestor.y_coord), xytext=(repeated_ancestors[ancestor.get_id().get_value()][0], repeated_ancestors[ancestor.get_id().get_value()][1]),
+                    arrowprops=dict(facecolor='gray', alpha=0.3))
+    else:
+        repeated_ancestors.add(ancestor.get_id().get_value())
+
+# Output the plot to a folder
+if args.out:
+    if not os.path.exists(args.out):
+        os.mkdir(args.out)
+    if args.format in ("pdf", "both"):
+        plt.savefig(args.out + "/family_tree.pdf")
+else:
+    if args.format in ("pdf", "both"):
+        plt.show()
+
+# create a text file based family tree
+if args.format in ("text", "both"):
+    with open(args.out + "/family_tree.txt", "w") as f:
+        for ancestor in ancestors:
             birth_date = ancestor.get_birth_date().get_value() if ancestor.get_birth_date() else 'N/A'
             birth_place = ancestor.get_birth_place().get_value() if ancestor.get_birth_place() else 'N/A'
             marriage_date = ancestor.get_marriage_date().get_value() if ancestor.get_marriage_date() else 'N/A'
             marriage_place = ancestor.get_marriage_place().get_value() if ancestor.get_marriage_place() else 'N/A'
             death_date = ancestor.get_death_date().get_value() if ancestor.get_death_date() else 'N/A'
             death_place = ancestor.get_death_place().get_value() if ancestor.get_death_place() else 'N/A'
-            ax.text(ancestor.x_coord, ancestor.y_coord, ancestor.name + ": " + birth_date + ' ' + birth_place + ' ' + marriage_date + ' ' + marriage_place + ' ' + death_date + ' ' + death_place)
+            f.write(ancestor.get_name().get_value() + ": " + birth_date + ' ' + birth_place + ' ' + marriage_date + ' ' + marriage_place + ' ' + death_date + ' ' + death_place + "\n")
 
-    # Handle repeated ancestors due to endogamy
-    repeated_ancestors = set()
-    for ancestor in ancestors:
-        if ancestor.get_id().get_value() in repeated_ancestors:
-            # Link ancestor to itself with a faint line
-            ax.annotate("", xy=(ancestor.x_coord, ancestor.y_coord), xytext=(repeated_ancestors[ancestor.get_id().get_value()][0], repeated_ancestors[ancestor.get_id().get_value()][1]),
-                        arrowprops=dict(facecolor='gray', alpha=0.3))
-        else:
-            repeated_ancestors.add(ancestor.get_id().get_value())
+# create more detailed version of the text file
+if args.format in ("text", "both"):
+    with open(args.out + "/family_tree_detailed.txt", "w") as f:
+        for ancestor in ancestors:
+            birth_date = ancestor.get_birth_date().get_value() if ancestor.get_birth_date() else 'N/A'
+            birth_place = ancestor.get_birth_place().get_value() if ancestor.get_birth_place() else 'N/A'
+            marriage_date = ancestor.get_marriage_date().get_value() if ancestor.get_marriage_date() else 'N/A'
+            marriage_place = ancestor.get_marriage_place().get_value() if ancestor.get_marriage_place() else 'N/A'
+            death_date = ancestor.get_death_date().get_value() if ancestor.get_death_date() else 'N/A'
+            death_place = ancestor.get_death_place().get_value() if ancestor.get_death_place() else 'N/A'
+            f.write(ancestor.get_name().get_value() + '\n')
+            f.write('Birth: '+ birth_date + ' ' + birth_place + '\n')
+            f.write('Marriage: '+ marriage_date + ' ' + marriage_place + '\n')
+            f.write('Death: '+ death_date + ' ' + death_place + '\n\n')
 
-    # Output the plot to a folder
-    if args.out:
-        if not os.path.exists(args.out):
-            os.mkdir(args.out)
-        if args.format in ("pdf", "both"):
-            plt.savefig(args.out + "/family_tree.pdf")
-            print(f"Saved family tree plot to {args.out}/family_tree.pdf")
-        if args.format in ("text", "both"):
-            with open(args.out + "/family_tree.txt", "w") as f:
-                for ancestor in ancestors:
-                    f.write(ancestor.get_name().get_value() + ": " + birth_date + ' ' + birth_place + ' ' + marriage_date + ' ' + marriage_place + ' ' + death_date + ' ' + death_place + "\n")
-                print(f"Saved family tree text file to {args.out}/family_tree.txt")
-    else:
-        if args.format in ("pdf", "both"):
-            plt.show()
-            print("Displaying family tree plot.")
-        if args.format in ("text", "both"):
-            with open("family_tree.txt", "w") as f:
-                for ancestor in ancestors:
-                    f.write(ancestor.get_name().get_value() + ": " + birth_date + ' ' + birth_place + ' ' + marriage_date + ' ' + marriage_place + ' ' + death_date + ' ' + death_place + "\n")
-                print("Saved family tree text file to family_tree.txt")
-except Exception as e:
-    print("An error occurred: ", e)
