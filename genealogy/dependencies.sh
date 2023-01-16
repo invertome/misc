0aba1753b2f966986e0dcf9637a5dc5e1eb3201c
InverTome <104219631+invertome@users.noreply.github.com>
2023-01-16 10:33:23 -0700

    Update dependencies.sh

--- genealogy/dependencies.sh
+++ genealogy/dependencies.sh
@@ -8,3 +8,12 @@ for package in "${dependencies[@]}"
 do
     mamba install -y $package -n $environment_name
 done
+for package in "${dependencies[@]}"
+do
+    if conda list -n $environment_name | grep -q $package; then
+        echo "$package is already installed."
+    else
+        echo "$package is not installed. Trying to install with pip..."
+        pip install --user $package
+    fi
+done
