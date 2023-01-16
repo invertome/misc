53861ac7e16d481438a8f652a4c385222a47d242
InverTome <104219631+invertome@users.noreply.github.com>
2023-01-16 10:43:03 -0700

    Update dependencies.sh

--- genealogy/dependencies.sh
+++ genealogy/dependencies.sh
@@ -3,7 +3,7 @@ dependencies=("ged4py" "matplotlib")
 environment_name="plotged"
 
 mamba create -y -n $environment_name
-mamba activate $environment_name
+conda activate $environment_name
 for package in "${dependencies[@]}"
 do
     mamba install -y $package -n $environment_name
