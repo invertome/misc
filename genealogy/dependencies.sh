ace1b3a7522634fc8bf7502361ea46253d4ab6d1
InverTome <104219631+invertome@users.noreply.github.com>
2023-01-16 10:05:07 -0700

    Create dependencies.sh

---
+++ genealogy/dependencies.sh
@@ -0,0 +1,9 @@
+# create new environment
+conda create --name plotged
+
+# activate the environment
+conda activate plotged
+
+# install gedcom and matplotlib
+conda install -c conda-forge gedcom matplotlib
+
